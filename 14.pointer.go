71fd789d116c2ec693c00dddb8758f959b694467
balinux <ryojuniyantara@gmail.com>
2024-07-10 07:00:00 +0800

    pointer

--- 14.pointer.go
+++ 14.pointer.go
@@ -11,4 +11,32 @@ func main() {
 
 	fmt.Println("varB value: ", *variabelB)
 	fmt.Println("varB address: ", variabelB)
+
+	// efek perubahan nilai pointer
+	var numberA int = 4
+	var numberB *int = &numberA
+
+	fmt.Println("nilai numberA", numberA)
+	fmt.Println("address numberA", &numberA)
+	fmt.Println("nilai numberB", *numberB)
+	fmt.Println("nilai numberB", numberB)
+
+	fmt.Println("")
+	numberA = 6
+
+	fmt.Println("nilai numberA", numberA)
+	fmt.Println("address numberA", &numberA)
+	fmt.Println("nilai numberB", *numberB)
+	fmt.Println("nilai numberB", numberB)
+
+	// pointer sebagai parameter fungsi
+	var numberParam int = 4
+	fmt.Println("number before change: ", numberParam)
+
+	change(&numberParam, 10)
+	fmt.Println("number after change: ", numberParam)
+}
+
+func change(original *int, value int) {
+	*original = value
 }
