f79ddd137f80de464be271ed7f16567d56dfe9e8
balinux <ryojuniyantara@gmail.com>
2024-07-15 09:09:58 +0800

    http template

---
+++ 37.http/http-template.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := map[string]string{
+			"Name":    "rio",
+			"Message": "belajar tempalte golang web",
+		}
+		t, err := template.ParseFiles("template.html")
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		t.Execute(w, data)
+	})
+
+	fmt.Println("starting webs erver at localhost:8000")
+	http.ListenAndServe(":8000", nil)
+}
