5dc4288329c88b8f4670d216ab52db908b4402da
balinux <ryojuniyantara@gmail.com>
2024-07-15 11:46:05 +0800

    rest api server

---
+++ 40.web-service-API-server.go
@@ -0,0 +1,68 @@
+package main
+
+import "encoding/json"
+import "net/http"
+import "fmt"
+
+type student struct {
+	ID    string
+	Name  string
+	Grade int
+}
+
+var data = []student{
+	student{"E001", "ethan", 21},
+	student{"W001", "wick", 22},
+	student{"B001", "bourne", 23},
+	student{"B002", "bond", 23},
+}
+
+func main() {
+	http.HandleFunc("/users", users)
+	http.HandleFunc("/user", user)
+	fmt.Println("starting API server at port 8000")
+	http.ListenAndServe(":8000", nil)
+}
+
+// menampilkan daftar users
+func users(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "GET" {
+		r, e := json.Marshal(data)
+
+		if e != nil {
+			fmt.Println(e.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(r)
+		return
+	}
+
+	http.Error(w, "", http.StatusBadRequest)
+}
+
+// mengambil 1 user saja
+func user(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	if r.Method == "GET" {
+		var id = r.FormValue("id")
+		var result []byte
+		var err error
+
+		for _, each := range data {
+			if each.ID == id {
+				result, err = json.Marshal(each)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+				}
+				w.Write(result)
+				return
+			}
+		}
+		http.Error(w, "user not found", http.StatusNotFound)
+	}
+	http.Error(w, "", http.StatusBadRequest)
+}
