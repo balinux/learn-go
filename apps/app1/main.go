9dd339c3754c4459225dd776c04cf282f1b5493a
rio <rio@news-MacBook-Air.local>
2024-07-28 11:53:19 +0800

    memindahkan file berdasarkan ekstensi

--- apps/app1/main.go
+++ apps/app1/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -15,28 +16,76 @@ func main() {
 	}
 	fmt.Println(currentDir)
 
+	// mendapatkan nama file yang desang di jalankan
+	executable, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	executableName := filepath.Base(executable)
+
 	// membukan dan membaca file di folder saat ini
 	files, err := os.ReadDir(currentDir)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// menghitung jumlah file berdasarkan extensi
-
-	fileExtentions := make(map[string]int)
-	fmt.Println("fileExtentions sebelum di isi", fileExtentions)
+	// memindahkan file ke folder berdasarkan ekstensi
 	for _, file := range files {
-		if !file.IsDir() {
+		if !file.IsDir() && file.Name() != executableName {
 			ext := filepath.Ext(file.Name())
-			fmt.Println("ekstensi", fileExtentions[ext])
-			fileExtentions[ext]++
+			if ext == "" {
+				fmt.Println("file has no extention")
+				// jika tidak memiliki ekstensi maka berikan nama folder no_extension
+				ext = "no_extension"
+			} else {
+				fmt.Println("ekstensi file", ext[1:])
+				ext = ext[1:]
+			}
+
+			// membaut folder berdasarkan ekstensi jika belum ada
+			extDir := filepath.Join(currentDir, ext)
+			if _, err := os.Stat(extDir); os.IsNotExist(err) {
+				err := os.Mkdir(extDir, 0755)
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+
+			// memindahkan file ke folder yang sesuai berdasarkan path
+			oldPath := filepath.Join(currentDir, file.Name())
+			newPath := filepath.Join(extDir, file.Name())
+			err := moveFile(oldPath, newPath)
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	}
-	fmt.Println("fileExtentions setelah di isi", fileExtentions)
+}
 
-	// menampilakn jumlah file
-	fmt.Printf("jumlah file di dalam folder %s \n", currentDir)
-	for ext, count := range fileExtentions {
-		fmt.Printf("%s:%d\n", ext, count)
+// memindahkan file dari oldpath ke new path
+func moveFile(oldPath, newPath string) error {
+	inputFile, err := os.Open(oldPath)
+	if err != nil {
+		return err
 	}
+	defer inputFile.Close()
+
+	outputFile, err := os.Create(newPath)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	// bagian untuk mengcopy file
+	_, err = io.Copy(outputFile, inputFile)
+	if err != nil {
+		return err
+	}
+
+	// hapus file
+	err = os.Remove(oldPath)
+	if err != nil {
+		return err
+	}
+	return nil
 }
