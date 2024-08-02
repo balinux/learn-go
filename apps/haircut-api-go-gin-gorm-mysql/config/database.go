71f691afd5c4ef915d22c8abef31b505738f1b02
rio <rio@news-MacBook-Air.local>
2024-08-02 19:38:15 +0800

    change database

--- apps/haircut-api-go-gin-gorm-mysql/config/database.go
+++ apps/haircut-api-go-gin-gorm-mysql/config/database.go
@@ -1,20 +1,21 @@
 package config
 
 import (
+	"log"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"log"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "root:1234Usd!@tcp(127.0.0.1:3306)/haircut?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "balinuxsv1:123B@l!nuxV1@tcp(127.0.0.1:3306)/haircut?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "root:1234Usd!@tcp(127.0.0.1:3306)/haircut?charset=utf8mb4&parseTime=True&loc=Local"
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	DB = database
-
 }
