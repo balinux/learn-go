23070db37d965b499cbad44e18622020cea99407
balinux <ryojuniyantara@gmail.com>
2024-08-06 16:25:22 +0000

    changen address database

--- apps/haircut-api-go-gin-gorm-postgres/config/database.go
+++ apps/haircut-api-go-gin-gorm-postgres/config/database.go
@@ -16,7 +16,7 @@ func ConnectDatabase() {
 	// dsn := "balinuxsv1:123B@l!nuxV1@tcp(172.16.211.109:3306)/haircut?charset=utf8mb4&parseTime=True&loc=Local"
 
 	// postgres
-	dsn := "host=172.16.211.109 user=balinux password=ninja dbname=haircut port=5432 sslmode=disable"
+	dsn := "host=100.72.100.119 user=balinux password=ninja dbname=haircut port=5432 sslmode=disable"
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
